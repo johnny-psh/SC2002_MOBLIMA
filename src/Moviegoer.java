3ed4fd2cfe54c305c82954cb836559f4b068f590
CY <chengyinlye@gmail.com>
2022-10-15 23:06:45 +0800

    Updated

--- src/Moviegoer.java
+++ src/Moviegoer.java
@@ -1,16 +1,37 @@
 public class Moviegoer {
+    /*
     //If there is login
     private String mUser; //username
-    private String mPass; //password
+    private String mPass; //password 
+    */
 
     //Moviegoer info
-    private String Name;
-    private int mobileNo;
-    private String email;
-    private int age;
+    private String Name,email;
+    private int mobileNo,age;
 
-    public Moviegoer() {
-        
+    public Moviegoer(String name,int mobile,String mail) {
+        this.Name = name;
+        this.mobileNo = mobile;
+        this.email = mail;
     }
+
+    public String getName()
+    {
+        return this.Name;
+    }
+    public String getEmail()
+    {
+        return this.email;
+    }
+    public int getPhone()
+    {
+        return this.mobileNo;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+    
     
 }
