8389a43618710d9dd8bf714557e5f088ee0aa53f
CY <chengyinlye@gmail.com>
2022-10-15 22:46:47 +0800

    Temporarily updated moviegoer class

---
+++ src/Moviegoer.java
@@ -0,0 +1,16 @@
+public class Moviegoer {
+    //If there is login
+    private String mUser; //username
+    private String mPass; //password
+
+    //Moviegoer info
+    private String Name;
+    private int mobileNo;
+    private String email;
+    private int age;
+
+    public Moviegoer() {
+        
+    }
+    
+}
