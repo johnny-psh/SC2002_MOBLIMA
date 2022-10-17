32799361b620a51d9d9850134522b65375e3b8f3
Shing Hao <46815846+shinghao@users.noreply.github.com>
2022-10-17 19:35:49 +0800

    Updated Rating Class
    - Renamed rating to review

---
+++ src/Review.java
@@ -0,0 +1,23 @@
+public class Review {
+
+    /* This class is called using <addReview> function of <Movie> class */
+
+    private String description;
+    private int rating;
+
+    public Review(String description, int rating)
+    {
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public int getRating()
+    {
+        return this.overallRating;
+    }
+    
+    public String getDescription()
+    {
+        return this.description;
+    }
+}
