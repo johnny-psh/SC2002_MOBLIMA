2e5d39d56bfe2fee95a06b2c592bbcddcfd1e02a
Johnny Pan <ultimatejp88@gmail.com>
2022-10-18 09:19:39 +0800

    Seat and Ticket

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,11 +1,19 @@
 public class Cinema{
 
-    private Seat seat;
+    private Seat[][] seats;
     private Movie movie;
 
     public Cinema(){
-        this.seat = new Seat();
         this.movie = new Movie();
+
+        this.seats = new Seat[5][10];
+        Ticket newTicket = new Ticket();
+
+        for(int i = 0; i < 5; i++){
+            for(int a = 0; a < 10; a++){
+                seats[i] = newTicket;
+            }
+        }
     }
     
 }
