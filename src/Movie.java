5e830f47dd13e1a31380088bcfc49d5865467648
CY <chengyinlye@gmail.com>
2022-10-19 16:20:14 +0800

    Temp Function for Txt File Reader

--- src/Movie.java
+++ src/Movie.java
@@ -186,24 +186,48 @@ public class Movie {
         }
     }
 
-    public void showTest() throws FileNotFoundException {
-        Scanner s = new Scanner(new File("C:/Users/User/Desktop/SC2002_Assignment/src/database/testData.txt"));
-List<String> names = new ArrayList<String>();
+    
+//Temporary Txt File Reader 
+    public static void main(String[] args) throws IOException {        
+       
 
-// Skip column headings.
 
-// Read each line, ensuring correct format.
-while (s.hasNext())
-{
-    s.nextInt();         // read and skip 'id'
-    names.add(s.next()); // read and store 'name'
-    s.nextInt();         // read and skip 'age'
-}
+ BufferedReader br = null;
+    String[] characters = new String[1024];//just an example - you have to initialize it to be big enough to hold all the lines!
 
-for (String name: names)
-{
-    System.out.println(name);
-}
+    try {
+
+        String sCurrentLine;
+        br = new BufferedReader(new FileReader("C:/Users/User/Desktop/SC2002_Assignment/src/database/testData.txt"));
+
+        int i=0;
+        while ((sCurrentLine = br.readLine()) != null) {
+            String[] arr = sCurrentLine.split(" ");
+            //for the first line it'll print
+            System.out.println("arr[0] = " + arr[0]); // h
+            System.out.println("arr[1] = " + arr[1]); // Vito
+            System.out.println("arr[2] = " + arr[2]); // 123
+            if(arr.length == 4){
+                System.out.println("arr[3] = " + arr[3]);
+            }
+
+            //Now if you want to enter them into separate arrays
+            characters[i] = arr[0];
+            // and you can do the same with
+            // names[1] = arr[1]
+            //etc
+            i++;
+        }
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (br != null)br.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-
+ 
+    }
 }
