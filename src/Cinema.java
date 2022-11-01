0435a7eb8c60ac20632c7953c4c0bb6528d57367
shinghao <sohshinghao@gmail.com>
2022-11-02 00:48:14 +0800

    Cinema Isle

--- src/Cinema.java
+++ src/Cinema.java
@@ -20,18 +20,25 @@ public class Cinema{
     }
 
     public void printCinemaLayout(){
+        int mid = this.numOfCols / 2;
+
         System.out.println("\nCinema " + this.cinemaID + "'s Seating Layout: \n");
         System.out.print(" \t");
 
         // Column numbering headers
-        for(int i = 1; i <= this.numOfRows; i++)
+        for(int i = 1; i <= this.numOfRows; i++){
             System.out.print("  " + i + "  ");
+            if(i == mid)
+                System.out.print("    "); // Isle
+        }
         System.out.print("\n");
         // Print each row of seats and it's respective row alphabet. Mark X if seat occupied.
         for(int i = this.numOfRows; i >= 1; i--){
             System.out.print((char) (i+64) + "\t"); //Row alphabet
             for(int j = 1; j <= this.numOfCols; j++){
                 System.out.print(this.seats[i-1][j-1].getIsOccupied() ? " [X] " : " [ ] ");
+                if(j == mid)
+                    System.out.print("    "); // Isle
             }
             System.out.println("\t" + (char) (i+64)); //Row alphabet
         }
@@ -72,7 +79,6 @@ public class Cinema{
 
     // For testing purposes - Delete afterwards
     // This is how booking seat will be implemented in booking class
-    
     // public static void main (String[] args){
     //     Cinema cin = new Cinema("HallA", Enums.CinemaType.REGULAR, 10, 10);
         
