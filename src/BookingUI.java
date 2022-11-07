e839e54fd8ae25b4ebd134e440695754b80e97f0
shinghao <sohshinghao@gmail.com>
2022-11-08 01:05:23 +0800

    Working BookingUI

--- src/BookingUI.java
+++ src/BookingUI.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.text.SimpleDateFormat;  
 import java.util.Date;  
 
 public class BookingUI {
@@ -12,9 +11,6 @@ public class BookingUI {
         Date selectedDate;
         Showtime selectedShowtime;
         int numOfTickets = 0;
-        ArrayList<Showtime> showtimeByCineplex = new ArrayList<Showtime>();
-        ArrayList<Showtime> showtimeByDate = new ArrayList<Showtime>();
-        ArrayList<Showtime> showtimeByMovie = new ArrayList<Showtime>();
 
         // 1. Select Cineplex
 
@@ -29,7 +25,7 @@ public class BookingUI {
         System.out.print("\nNumbers of tickets to purchase: ");
         numOfTickets = scanner.nextInt();
         // 6. Select seats
-        selectSeats(numOfTickets, selectedShowtime.getCinema());
+        selectSeats(numOfTickets, selectedShowtime);
         // 7. Transaction
         completeTransaction(numOfTickets, selectedShowtime.getCinema());
     }
@@ -43,7 +39,8 @@ public class BookingUI {
         if(numOfCineplexes == 0)
             return null;
 
-		while(userOption != (numOfCineplexes+1)) {
+		do {
+            System.out.println("\nSelect Cineplex: ");
             for(int i = 0; i < numOfCineplexes; i++) {
                 System.out.println((i+1) + ". " + cineplexList.get(i).getCineplexeName());
             }
@@ -52,13 +49,13 @@ public class BookingUI {
 			userOption = scanner.nextInt();
 
 			// Back
-			if(userOption >= numOfCineplexes){
+			if(userOption < 1 || userOption >= (numOfCineplexes+1)){
 				System.out.println("Invalid Option! Please re-enter.");
 				System.out.println("Please re-enter.");
                 break;
             }
-        }
-        return (cineplexList.get(userOption));
+        } while(userOption < 1 || userOption >= (numOfCineplexes+1));
+        return (cineplexList.get(userOption-1));
     }
 
     private static Date selectDate(Cineplex cineplex){
@@ -70,23 +67,23 @@ public class BookingUI {
         if(numOfShowtimes == 0)
             return null;
 
-		while(userOption >= (numOfShowtimes+1)) {
-            System.out.println("Select Date: ");
-            for(int i = 1; i <= showtimeList.size(); i++){
-                System.out.println(i + "." + showtimeList.get(i).getFormattedDate());
+		do {
+            System.out.println("\nSelect Date: ");
+            for(int i = 0; i < showtimeList.size(); i++){
+                System.out.println((i+1) + ". " + showtimeList.get(i).getFormattedDate());
             }
             System.out.println((numOfShowtimes+1) + ". Back");
 		    System.out.print("Option > ");
 			userOption = scanner.nextInt();
 
             // Back
-			if(userOption >= (numOfShowtimes+1)){
+			if(userOption < 1 || userOption >= (numOfShowtimes+1)){
 				System.out.println("Invalid Option!");
 				System.out.println("Please re-enter!");
                 break;
             }
-        }
-        return (showtimeList.get(userOption).getDate());
+        } while(userOption < 1 || userOption >= (numOfShowtimes+1));
+        return (showtimeList.get(userOption-1).getDate());
     }
 
     private static Showtime selectShowtime(Cineplex cineplex, Date date){
@@ -97,9 +94,10 @@ public class BookingUI {
 
         if(numOfShowtimes == 0)
             return null;
-        while(userOption >= (numOfShowtimes+1)) {
-            System.out.println("Select Date: ");
-            for(int i = 1; i <= showtimeList.size(); i++){
+        do {
+            System.out.println("\nSelect Movie and Showtime: ");
+            for(int i = 0; i < showtimeList.size(); i++){
+                System.out.print((i+1) + ". ");
                 showtimeList.get(i).printShowtime();
             }
             System.out.println((numOfShowtimes+1) + ". Back");
@@ -107,46 +105,106 @@ public class BookingUI {
             userOption = scanner.nextInt();
     
             // Back
-            if(userOption >= (numOfShowtimes+1)){
+            if(userOption < 1 || userOption >= (numOfShowtimes+1)){
                 System.out.println("Invalid Option!");
                 System.out.println("Please re-enter!");
                 break;
             }
-        }                       
-        return (showtimeList.get(userOption));
+        } while(userOption >= (numOfShowtimes+1));            
+        return (showtimeList.get(userOption-1));
     }
     
     private static void viewSeats(Cinema cinema){
         cinema.printCinemaLayout();
     }
 
-    private static void selectSeats(int numOfTickets, Cinema cinema){
+    private static void selectSeats(int numOfTickets, Showtime showtime){
+        Cinema cinema = showtime.getCinema();
+        ArrayList<Ticket> ticketList = new ArrayList<Ticket>(numOfTickets);
         char row;
         int col;
         boolean isOccupiedSeat = false;
+        double totalPrice = 0;
         for(int i = 0; i < numOfTickets; i++){
             do{
-                System.out.println("Ticket (" + (i+1) + ")");
+                System.out.println("\nSelect Ticket (" + (i+1) + "):");
                 System.out.print("Row: ");
                 row = scanner.next().charAt(0);
+                row = Character.toUpperCase(row);
                 System.out.print("Column: ");
                 col = scanner.nextInt();
                 if(!cinema.getSeatOccupied(row, col)){
-                   isOccupiedSeat = false;
-                   break;
+                    Enums.TypeOfMovieGoer movieGoerType = selectTypeOfMovieGoer();
+                    Seat seat = cinema.getSeat(row,col);
+                    Ticket ticket = new Ticket(seat,  showtime, movieGoerType);
+                    System.out.println("Price: " + ticket.getTicketPrice());
+                    totalPrice += ticket.getTicketPrice();
+                    isOccupiedSeat = false;
+                    ticketList.add(ticket);
+                    break;
                 }
                 System.out.println("Seat selected is occupied. Please select another seat");
                 isOccupiedSeat = true;
-            }while(isOccupiedSeat);
+            } while(isOccupiedSeat);
         }
+        System.out.println("\nTotal price is: " + totalPrice + " SGD");
+    	System.out.print("Do you want to confirm payment? Yes (1) / No (2): ");
+    	int userOption = scanner.nextInt();
+    	if(userOption == 1){
+    		saveSeatToDB();
+            saveBookingToDB();
+		}
+    	else{
+    		System.out.println("\nReturning to main menu...\n");
+    		return;
+		}
+    }
+
+    // Function to save reserved seats to database from CinemaController.java
+    private static void saveSeatToDB(){
+        return;
+    }
+
+    // Function to save booking to user database from userBookings.java
+    private static void saveBookingToDB(){
+        return;
+    }
+
+    private static Enums.TypeOfMovieGoer selectTypeOfMovieGoer(){
+        int userOption = 0;
+
+		do {
+            System.out.println("\nSelect ticket type: ");
+            System.out.println("1. Student");
+            System.out.println("2. Adult");            
+            System.out.println("3. Senior Citizen");
+		    System.out.print("Option > ");
+			userOption = scanner.nextInt();
+
+            // Back
+			switch (userOption) {
+                case 1:
+                    return Enums.TypeOfMovieGoer.STUDENT;
+                case 2:
+                    return Enums.TypeOfMovieGoer.ADULT;
+                case 3:
+                    return Enums.TypeOfMovieGoer.SENIOR;
+                default:
+                    System.out.println("Invalid Option!");
+				    System.out.println("Please re-enter!");
+                    break;
+            }
+        } while(userOption < 1 || userOption > 3);
+        return null;
     }
 
     private static void completeTransaction(int numOfTickets, Cinema cinema){
-        // Make Payment
-
+        // TODO: Capture movie-goer's name, mobile number, email address
+        
         // Transaction
         Transaction transaction = new Transaction(cinema.getCinemaID());
-        System.out.println("You have successfully purchased " + numOfTickets + " ticket(s)");
+        System.out.println("\nYou have successfully purchased " + numOfTickets + " ticket(s)");
         System.out.println("Transaction ID: " + transaction.getTransactionID());
+        System.out.println("\nReturning to main menu...\n");
     }
 }
