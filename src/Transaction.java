706f7b03ca789ca5a2687702f9bb1cd5dca87280
shinghao <sohshinghao@gmail.com>
2022-11-08 01:42:09 +0800

    Update Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -5,8 +5,14 @@ public class Transaction {
     private String transactionID; // TID - XXXYYYYMMDDhhmm
     private String cinemaID;
     private Date date;
+    private String username;
+    private Showtime showtime;
 
-    public Transaction(String cinemaID){
+    public Transaction(Showtime showtime, String username){
+        this.showtime = showtime;
+        this.username = username;
+
+        this.cinemaID = showtime.getCinema().getCinemaID();
         SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("yyyyMMddHHmm");  
         this.date = new Date();  
         this.transactionID = "" + cinemaID + dateTimeFormatter.format(this.date);
@@ -15,8 +21,17 @@ public class Transaction {
     public String getTransactionID(){
         return this.transactionID;
     }
-    
-    public String getCinemaID(){
-        return this.cinemaID;
-    } 
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String toString(){
+        String toReturn = "";
+		toReturn 	+= "Transaction ID: " + this.getTransactionID() + "\n"
+					+ "Movie: " + this.showtime.getMovie().getTitle() + "\n"
+					+ "Movie Date: " + this.showtime.getFormattedDate() + "\n"
+					+ "Movie Time: " + this.showtime.getFormattedTime() + "\n\n";
+		return toReturn; 
+    }
 }
