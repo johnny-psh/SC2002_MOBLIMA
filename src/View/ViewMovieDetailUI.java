19c0600fade5cff42a0cd750444b173631e252fc
shinghao <sohshinghao@gmail.com>
2022-11-12 14:56:04 +0800

    Update ViewMovieDetailUI.java

--- src/View/ViewMovieDetailUI.java
+++ src/View/ViewMovieDetailUI.java
@@ -9,20 +9,22 @@ public class ViewMovieDetailUI {
     static Scanner scanner = new Scanner(System.in);
    
     public static void displayMenu() throws IOException{
-
-        System.out.print("\nEnter movie ID to view movie detail: ");
-        String movieID = scanner.next();
-       
-        Movie movie = MoviesController.readByID(movieID);
-        if(movie == null){
-            System.out.println("Movie with this id doesn't exist!\n");
-        }
-        else{
-            System.out.println("");
-            MovieDetailsPrinter movieDetailsPrinter = new MovieDetailsPrinter(movie);
-            movieDetailsPrinter.printMovieDetails();
-        }
-
+        boolean valid = false;
+        do{
+            System.out.print("\nEnter movie ID to view movie detail: ");
+            String movieID = scanner.next();
+        
+            Movie movie = MoviesController.readByID(movieID);
+            if(movie == null){
+                System.out.println("\nMovie with this id does not exist! Please try keying in another movie ID.");
+            }
+            else{
+                System.out.println("");
+                MovieDetailsPrinter movieDetailsPrinter = new MovieDetailsPrinter(movie);
+                movieDetailsPrinter.printMovieDetails();
+                valid = true;
+            }
+        } while(!valid);
         ExitUI.displayMenu();
         return; 
     }
